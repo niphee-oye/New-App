907fe30fac89262364d3e4dbd25beff140d28a6f
niphee-oye <oluwanifemi.oyewole@pau.edu.ng>
2022-06-20 23:39:27 +0100

    Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentRegistrationModule } from './student-registration/student-registration.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+  type: "postgres",
+  host: "localhost",
+  port: 5000,
+  username: "new-app",
+  password: "theapp",
+  database: "New-App",
+  entities: [
+  "dist/**/*.entity{.ts,.js}"
+  ],
+  synchronize: false
+  }), StudentRegistrationModule],
   controllers: [AppController],
   providers: [AppService],
-})
-export class AppModule {}
+  })
+  export class AppModule {}
