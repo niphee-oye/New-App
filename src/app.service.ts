5cb5d9da5b8a0f94997b1cf3bedc364945d20e47
niphee-oye <oluwanifemi.oyewole@pau.edu.ng>
2022-06-20 22:11:05 +0100

    New App things

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hey there!';
   }
+  getHello2(): {} {
+    return {message: 'Hello People!', title: 'My Greeting App'};
+  }
+  getHome(): {} {
+  return {title: 'Home Page'};
+  }
+  getAboutUs(): {} {
+  return {title: 'About Us Page'};
+  }
 }
